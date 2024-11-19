1091ef82f5577e315e9a89a6a774377f53ac2815
LuigiRac <lui.rac06@gmail.com>
2024-11-19 12:16:24 +0100

    start exercise

---
+++ script/script.js
@@ -0,0 +1,9 @@
+"use strict"
+console.clear();
+
+
+const baseUrl = "https://jsonplaceholder.typicode.com/";
+const resource = "photos";
+
+const endPoint = baseUrl + resource;
+console.log(endPoint);
