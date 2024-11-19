7b1f3bbc278c7e2acc539479c2a99e626a9c0ba2
LuigiRac <lui.rac06@gmail.com>
2024-11-19 13:12:24 +0100

    header

--- script/script.js
+++ script/script.js
@@ -5,5 +5,11 @@ console.clear();
 const baseUrl = "https://jsonplaceholder.typicode.com/";
 const resource = "photos";
 
-const endPoint = baseUrl + resource;
-console.log(endPoint);
+// const endPoint = baseUrl + resource;
+const params = { "_limit": 6 };
+// console.log(endPoint + "?_limit=6");
+
+
+axios.get(baseUrl + resource, { params }).then((res) => {
+    console.log(res);
+});
