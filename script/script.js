665a7a3453f15027e921ea40007625fef9bd1a0e
LuigiRac <lui.rac06@gmail.com>
2024-11-20 20:09:16 +0100

    for userDAta

--- script/script.js
+++ script/script.js
@@ -8,13 +8,30 @@ const resource = "photos";
 // const endPoint = baseUrl + resource;
 const params = { "_limit": 6 };
 // console.log(endPoint + "?_limit=6");
-
+const boxCard = document.getElementById("container-card");
+// console.log(boxCard);
 
 axios.get(baseUrl + resource, { params }).then((res) => {
-    console.log(res.data);
+    // console.log(res.data);
+    const resData = res.data;
+
+    for (let i = 0; i < resData.length; i++) {
+        const userData = resData[i];
+        const card = `
+        <div class="card">
+                <img src="${userData.url}" alt="${userData.title}" style="width:100%">
+                <div class="card-container debug">
+                    <p>${userData.title}</p>
+                </div>
+            </div>`
+        boxCard.innerHTML += card;
+        console.log(userData);
+
+    }
+
+
 });
 
-const boxCard = document.getElementsByClassName("box-card");
-console.log(boxCard);
+
 
 
