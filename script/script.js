14f3545efe84eb72ad9c4c1b22d42c7b5e2e899e
LuigiRac <lui.rac06@gmail.com>
2024-11-24 15:53:33 +0100

    pin

--- script/script.js
+++ script/script.js
@@ -11,6 +11,10 @@ const params = { "_limit": 6 };
 const boxCard = document.getElementById("container-card");
 // console.log(boxCard);
 
+
+// button.addEventListener("click", getFigures);
+
+
 axios.get(baseUrl + resource, { params }).then((res) => {
     // console.log(res.data);
     const resData = res.data;
@@ -18,19 +22,26 @@ axios.get(baseUrl + resource, { params }).then((res) => {
     for (let i = 0; i < resData.length; i++) {
         const userData = resData[i];
         const card = `
-        <div class="card">
+        
+        <div id="id-figures" class="card">
+                <img class="pin" src="img/pin.svg" alt="">
                 <img src="${userData.url}" alt="${userData.title}" style="width:100%">
                 <div class="card-container debug">
+                
                     <p>${userData.title}</p>
                 </div>
             </div>`
         boxCard.innerHTML += card;
-        console.log(userData);
+        // console.log(userData);
 
     }
-
-
 });
+
+// function getFigures() {
+//     const figures = document.getElementById("id-figures");
+//     console.log(figures);
+
+// }
 
 
 
